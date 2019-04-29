58d5c5786c0f7d62b0a38fd632f6a5c6c73213cf
Aaron Jacobs <aaron.jacobs@crescendotechnology.com>
2019-04-29 20:18:33 +0000

    Overhauls the test suite to take advantage of reconnection support.
    We can now test errors that close the channel or connection easily.

--- tests/testthat/test-exchanges.R
+++ tests/testthat/test-exchanges.R
@@ -3,14 +3,24 @@ testthat::context("test-exchanges.R")
 testthat::test_that("Exchanges can be created and deleted", {
   conn <- amqp_connect()
 
-  # Create/immediately delete an exchange.
-  testthat::expect_silent(amqp_declare_exchange(conn, "test_exchange"))
-  testthat::expect_silent(amqp_delete_exchange(conn, "test_exchange"))
+  testthat::expect_silent(amqp_delete_exchange(conn, "test.exchange"))
+  testthat::expect_silent(amqp_declare_exchange(conn, "test.exchange"))
+
+  # Attempt to declare an exchange with an inconsistent type.
+  testthat::expect_error(
+    amqp_declare_exchange(conn, "test.exchange", type = "fanout"),
+    regexp = "PRECONDITION_FAILED"
+  )
+
+  testthat::expect_silent(amqp_declare_exchange(conn, "test.exchange"))
 
   # Attempt to create an exchange with an invalid type.
-  testthat::expect_error(amqp_declare_exchange(conn, "test_exchange",
-                                               type = "invalid"),
-                         regexp = "invalid exchange type")
+  testthat::expect_error(
+    amqp_declare_exchange(conn, "test.exchange2", type = "invalid"),
+    regexp = "invalid exchange type"
+  )
+
+  testthat::expect_silent(amqp_delete_exchange(conn, "test.exchange"))
 
   amqp_disconnect(conn)
 })
@@ -18,13 +28,17 @@ testthat::test_that("Exchanges can be created and deleted", {
 testthat::test_that("Queues can be bound to exchanges", {
   conn <- amqp_connect()
 
-  amqp_declare_exchange(conn, "test_exchange")
+  amqp_declare_exchange(conn, "test.exchange")
   tmp <- amqp_declare_tmp_queue(conn)
 
-  testthat::expect_silent(amqp_bind_queue(conn, tmp, "test_exchange", tmp))
-  testthat::expect_silent(amqp_unbind_queue(conn, tmp, "test_exchange", tmp))
+  testthat::expect_silent(amqp_bind_queue(conn, tmp, "test.exchange"))
+  testthat::expect_silent(amqp_unbind_queue(conn, tmp, "test.exchange"))
 
-  amqp_delete_exchange(conn, "test_exchange")
+  # Bind to a non-existant exchange.
+  testthat::expect_error(amqp_bind_queue(conn, tmp, "doesnotexist"),
+                         regexp = "NOT_FOUND")
+
+  amqp_delete_exchange(conn, "test.exchange")
   amqp_delete_queue(conn, tmp, if_empty = TRUE)
   amqp_disconnect(conn)
 })
