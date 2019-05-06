970f598a0990539ec5fc01365f5933a4c54c14ff
Aaron Jacobs <aaron.jacobs@crescendotechnology.com>
2019-05-06 17:54:27 +0000

    Implements as.list() to decode the output of amqp_properties().
    This seems like the idiomatic thing to do.

--- R/package.R
+++ R/package.R
@@ -2,14 +2,18 @@
 NULL
 
 #' @export
-amqp_properties <- function(props, ...) {
+amqp_properties <- function(...) {
   args <- list(...)
-  if (!missing(props) && inherits(props, "amqp_properties")) {
-    out <- .Call(R_amqp_decode_properties, props$ptr)
-    return(out)
-  }
   if (!all(vapply(args, nchar, integer(1)) > 0)) {
     stop("All properties must be named.")
   }
   .Call(R_amqp_encode_properties, args)
 }
+
+#' @param x An object of class \code{"amqp_properties"}.
+#' @rdname amqp_properties
+#' @export
+as.list.amqp_properties <- function(x, ...) {
+  .Call(R_amqp_decode_properties, x$ptr)
+}
+
