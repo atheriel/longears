82a03ec74da98c9975e9b93cc83cb8f40e5d6192
Aaron Jacobs <aaron.jacobs@crescendotechnology.com>
2019-05-08 17:27:56 +0000

    Adds print(), tests, and full documentation for amqp_properties().

---
+++ tests/testthat/test-properties.R
@@ -0,0 +1,18 @@
+testthat::context("test-properties")
+
+testthat::test_that("Encoding and decoding properties works correctly", {
+  # Empty properties.
+  props <- testthat::expect_silent(amqp_properties())
+  empty <- list()
+  names(empty) <- character(0)
+  testthat::expect_equal(as.list(props), empty)
+
+  valid_props <- list(
+    content_type = "text/plain", content_encoding = "UTF-8", delivery_mode = 2,
+    priority = 2, correlation_id = "2", reply_to = "1", expiration = "2020",
+    message_id = "2", type = "message", user_id = "guest",
+    app_id = "application", cluster_id = "cluster"
+  )
+  props <- testthat::expect_silent(do.call(amqp_properties, valid_props))
+  testthat::expect_equal(as.list(props), valid_props)
+})
